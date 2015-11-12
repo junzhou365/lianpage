d703d7e37fa8357543527787e476b9f2cd50c711
Julian Zhou <junzhou365@gmail.com>
2015-11-12 17:06:32 -0500

    Add contact page
    Fix minor bugs

--- app/app.js
+++ app/app.js
@@ -8,46 +8,32 @@ angular.module('myApp', ['ui.router'])
       templateUrl:'partials/main.html',
       controller:'mainController'
     })
-    .state('about', {
-      url:'/about',
+    .state('main.about', {
+      url:'about',
       templateUrl:'partials/about.html',
-      controller:'mainController'
+      controller:'aboutController'
     })
-    .state('contact', {
-      url:'/contact',
+    .state('main.contact', {
+      url:'contact',
       templateUrl:'partials/contact.html',
-      controller:'mainController'
-    })
-    .state('games', {
-      url:'/games',
-      templateUrl:'partials/games.html',
-      controller:'mainController'
+      controller:'contactController'
     })
     .state('projects', {
       url:'/projects',
       templateUrl:'partials/projects.html',
-      controller:'mainController'
+      controller:'projectsController'
     })
-    .state('frogger', {
-      url:'/frogger',
-      templateUrl:'partials/frogger.html',
-      controller:'mainController'
+    .state('resume', {
+      url:'/resume',
+      templateUrl:'partials/resume.html',
+      controller:'resumeController'
     })
  }).run(function($state){
    $state.go('main');
  })
 
-.controller('mainController', ['$scope', '$rootScope', '$window', '$state', function($scope,$rootScope,$window,$state){
-  // $("div").velocity({
-  //   width: "*=2"
-  // }, 2000);
-
-  controlVideo();
-  var barWidth = 200;
-  var barHeight = 50;
-  var lineWidth = 4;
-  var basics = { barWidth: barWidth, barHeight: barHeight, lineLength: 2 * barWidth, lineWidth: lineWidth }
-  // console.log(basics);
+.controller('mainController', ['$scope', '$rootScope', '$window', '$state', function($scope,$rootScope,$window,$state) {
+  var basics = 0;
   $(window).resize(function(){
 
     $scope.$apply(function(){
@@ -57,9 +43,81 @@ angular.module('myApp', ['ui.router'])
   });
   // setLayout(basics, $(window).width(), $(window).height())
 
+  $scope.jun = function() {
+    $state.go('main', {}, {reload:true});
+  };
   // $scope.about = selectSection(basics, $state, "about");
   // $scope.contact = selectSection(basics, $state, "contact");
   // $scope.games = selectSection(basics, $state, "games");
   // $scope.projects = selectSection(basics, $state, "projects");
 
+  controlVideo();
+  expandMenu();
+  console.log("main executed!");
+
+}])
+.controller('aboutController', ['$scope', '$interval', '$timeout', function($scope, $interval,$timeout) {
+  var sentences = $("p");
+  var size = sentences.length;
+  var baseDuration = 250;
+  // var getLineHeight = function(i) {return parseInt(sentences.eq(i).css("line-height").slice(0, -2)) + parseInt(sentences.eq(i).css("margin-bottom").slice(0, -2))}
+  var height =  sentences.eq(0).outerHeight(true) * (size-1) + sentences.eq(2).outerHeight(true) + $(".blanket").css("padding").slice(0, -2) * 2 + $(".link").css("padding").slice(0, -2) * 2;
+  $scope.roles = [false, false, false];
+
+  $("#aboutSection")
+  // .velocity({opacity: 1}, 500)
+  .velocity({height: height}, {duration : baseDuration * size, easing: [0,0,0,1]})
+  ;
+  for (var i = 0; i < size-1; i++) {
+    sentences.eq(i).velocity("fadeIn", {duration: baseDuration, delay: i * baseDuration});
+    // hard coded
+  }
+  sentences.eq((size-1)).velocity({opacity: 1}, {display: "inline-block", duration: baseDuration, delay: (size-1) * baseDuration });
+
+  $timeout(function() {
+    $scope.roles = [true, false, false];
+    $interval(function() {
+      $scope.roles = $scope.roles.slice(2,3).concat($scope.roles.slice(0,2));
+    }, baseDuration * 10)
+
+    $scope.$watch(function() { return $scope.roles; }, function(newValue, oldValue) {
+      for (var i = 0; i < 3; i++) {
+        if ($scope.roles[i] === true) {
+          $(".aboutRole").eq(i).velocity({opacity: 1}, {duration: baseDuration, easing: "easeIn"})
+                                .velocity({opacity: 0}, {duration: baseDuration, easing: "easeOut", delay: baseDuration * 8});
+          // $(".aboutRole").eq(i).velocity("slideDown", {duration: 1000});
+        }
+      }
+    })
+  }, baseDuration * 4);
+}])
+.controller('resumeController', ['$scope', function($scope) {
+  var config = {
+        easing: 'ease-in',
+        reset:  true,
+        delay:  'onload',
+        move:     '30px',
+        vFactor: 0.90
+      }
+  $(window).sr = new scrollReveal(config);
+}])
+.controller('projectsController', ['$scope', '$state', '$timeout', function($scope,$state,$timeout) {
+  var grid = $('.grid').masonry({
+    // options
+    itemSelector: '.grid-item',
+  // use element for option
+    columnWidth: '.grid-sizer',
+    percentPosition: true
+  });
+
+  grid.imagesLoaded().progress( function() {
+  grid.masonry('layout');
+});
+}])
+.controller('contactController', ['$scope', '$interval', '$timeout', function($scope, $interval,$timeout) {
+  var height = $("#contactSection").outerHeight(true);
+  $("#contactSection")
+  .css({"height": 0})
+  // .velocity({opacity: 1}, 500)
+  .velocity({height: height}, {duration : 500, easing: [0,0,0,1]});
 }]);
--- app/js/helper2.js
+++ app/js/helper2.js
@@ -1,14 +1,63 @@
+function vidFade(vid) {
+  vid.classList.add("stopfade");
+}
+
 var controlVideo = function() {
   var vid = document.getElementById("bgvid");
-  function vidFade() {
-    vid.classList.add("stopfade");
+  if (vid != null) {
+    vid.addEventListener('ended', function()
+    {
+      // only functional if "loop" is removed
+        vid.pause();
+      // to capture IE10
+        vidFade(vid);
+    });
   }
 
-  vid.addEventListener('ended', function()
-  {
-    // only functional if "loop" is removed
-      vid.pause();
-    // to capture IE10
-      vidFade();
-  });
+}
+
+var selectSection = function(basics, $state, section) {
+  return function() {
+    sections = ["about", "projects", "contact"].filter(function(x){ return x != section});
+    sections.splice(0, 0, section);
+    var durationStep1 = 500;
+    var durationStep2 = 500;
+    var redirectDelay = durationStep1 + durationStep2;
+    // $("#"+sections[0]).delay(durationStep1)
+    // .velocity({opacity: 0}, {duration: durationStep2, display: "none", easing: [0,0,1,1]});
+    // $("#"+sections[0]).css({borderWidth: 4});
+
+    // $("#jun").delay(durationStep1)
+    // .velocity({opacity: 0}, {duration: durationStep2, display: "none", easing: [0,0,1,1]});
+
+    // $("#"+sections[1])
+    // .velocity({opacity: 0}, {duration: durationStep1, display: "none"});
+    //
+    // $("#"+sections[2])
+    // .velocity({opacity: 0}, {duration: durationStep1, display: "none"});
+    //
+    // $("#"+sections[3])
+    // .velocity({opacity: 0}, {duration: durationStep1, display: "none"});
+
+    // $("#menu").delay(durationStep1)
+    // .velocity({opacity: 0}, {duration: durationStep2, display: "none"});
+    if (section == "projects") {
+      setTimeout(function(){ $state.go(section); }, redirectDelay);
+    } else {
+      setTimeout(function(){ $state.go("main." + section); }, redirectDelay);
+    }
+
+  }
+}
+
+function expandMenu() {
+  var height = $("#menu").outerHeight(true);
+  var sections = $("#menu").children();
+  var size = sections.length;
+  $("#menu").velocity({height: height}, {duration : 500 * size, easing: [0,0,0,1]})
+  for (var i = 0; i < sections.length; i++) {
+    sections.eq(i)
+    .velocity("fadeIn", {duration: 250, delay: i * 250});
+  }
+
 }
